df29fab2cc649a9427a1db77890e047e41424009
JackWhiskey <75497398+JackWhiskey@users.noreply.github.com>
2023-09-06 22:46:31 -0400

    Moved test file to __tests__ folder

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (or don't because Javascript sucks)
         </a>
       </header>
     </div>
