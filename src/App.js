55b99b9b68c1bcc57b42b6d7722543ebf00f1838
diegovaldiviac <valdiviacox.d@northeastern.edu>
2023-10-08 16:50:59 -0400

    google api key in html, points as props for map

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Map from "./components/Map2"
 import { platformClient } from './api/PlatformClient';
 import { Button, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+//import { google } from '@react-google-maps/api'; // or the appropriate import path
 
 
 const App = () => {
   const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+  const [points, setPoints] = useState(null)
 
   
   /**
@@ -28,21 +30,27 @@ const App = () => {
     console.log('Position Updated')
   };
 
-  const handleBackendRequest = async () => {
-    try {
-    const response = await platformClient.fetchData('capstone')
-    console.log(response)
-    } catch(error) {
-      console.log(error)
+  async function getPoints() {
+    const googleType = []
+      try {
+        const response = await platformClient.fetchData('capstone/points')
+        for (const coor of response) {
+          const latitude = coor[0];
+          const longitude = coor[1];
+          googleType.push(new google.maps.LatLng(latitude, longitude))
+        }
+        setPoints(googleType)
+      } catch(error) {
+        console.log(error)
+      }      
     }
-  }
 
   return (
     <div>
       <Typography>Latitude: {mapCenter.lat}</Typography>
       <Typography>Longitude: {mapCenter.lng}</Typography>
-      <Button onClick={handleBackendRequest} type="submit" variant="contained" color="secondary"> CLICK ME </Button>
-      <Map latitude={mapCenter.lat} longitude={mapCenter.lng}/>
+      <Button onClick={getPoints} type="submit" variant="contained" color="primary"> Request Readings </Button>
+      <Map latitude={mapCenter.lat} longitude={mapCenter.lng} points={points}/>
     </div>
   );
 };
